72d69dd4770bd660659e25873b4bb9a214c57f58
roach <tmdgusya@naver.com>
2021-12-23 04:31:22 +0900

    인프런 추천강의 추가

--- front/roach-books/src/page/data/inflearn.ts
+++ front/roach-books/src/page/data/inflearn.ts
@@ -15,4 +15,28 @@ export const inflearn: ItemType[] = [{
     photo: "https://user-images.githubusercontent.com/57784077/147139516-6433423e-bf90-43d5-9e21-555e6685f43a.png",
     descriptions: ["믿고 사는 영한님 강의인 만큼 설명이 매우 친절하다",
         "초보자가 이해할 수 있도록 설명하신다고는 하나 어느정도의 DB 지식은 알고 강의를 듣자. 그래야 더 잘이해간다."]
+}, {
+    title: "모든 개발자를 위한 HTTP 웹 기본 지식",
+    evaluation: 3,
+    href: "https://www.inflearn.com/course/http-%EC%9B%B9-%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/dashboard",
+    suggestion: "웹 개발자",
+    photo: "https://user-images.githubusercontent.com/57784077/147144435-cb46faf4-362d-47f5-8a6d-fb014cd45e9e.png",
+    descriptions: ["모르는 부분 몇개랑 초반부분을 골라서 들었는데 확실히 설명이 초보자가 이해하기에도 좋음.",
+        "이 강의로 간단하게 익힉고 나중에 추가적으로 다른 책으로 심화적으로 공부하는걸 추천"]
+}, {
+    title: "스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술",
+    evaluation: 3.5,
+    href: "https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-mvc-1/dashboard",
+    suggestion: "Spring 내부 동작원리를 만들어보면서 알고 싶은사람, 혹은 자바 백엔드 면접 준비생",
+    photo: 'https://user-images.githubusercontent.com/57784077/147144736-c72ed0a1-630b-413c-94a6-a4858dcf88e7.png',
+    descriptions: ["음 스프링 백엔드 면접을 준비하고 있다면 한번 쯤 듣는것이 좋다",
+    "자신이 사용하는 프레임워크가 어떻게 동작하고 어떻게 발전했는지 간단히 알려면 이 강의에 돈을쓰는게 시간절약 할수 있는 방법이다."]
+}, {
+    title: "실습으로 배우는 AWS 핵심 서비스",
+    evaluation: 3.5,
+    href: "https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-mvc-1/dashboard",
+    suggestion: "AWS 를 책으로 배우기엔 막막한데 실습으로 한번 따라해보면서 배우고 싶은 사람",
+    photo: "https://user-images.githubusercontent.com/57784077/147145335-b2bcb0c3-422c-4c4a-b3c0-bd1375ee7cd0.png",
+    descriptions: ["어느정도 네트워크에 대한 간단한 지식 정도와 AWS 서비스에 대한 간단한 지식을 가지고 들으면 좋음",
+        "AWS 같은 경우 책으로 좀 학습하기 힘들었는데 이해안가는 부분을 강의를 따라하는 실습형태로 공부하다보니 약간 편한느낌이 들었음."]
 }]
