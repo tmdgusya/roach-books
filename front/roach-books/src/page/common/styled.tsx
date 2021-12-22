c24a2b8d9fab1ab38e18d906509a72fe36fbc141
roach <tmdgusya@naver.com>
2021-12-23 02:39:21 +0900

    공통 스타일링 추가

---
+++ front/roach-books/src/page/common/styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const RoachBooksTitle = styled.h1`
+    size: 80px;
+`
+
+export const HeadLine = styled.h1`
+    
+    `
+
+export const MainDiv = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    `
